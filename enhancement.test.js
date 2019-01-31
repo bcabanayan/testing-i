baf566d27947212311aa172ab71621b9bd1f7f22
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-01-30 19:21:29 -0700

    Wrote fail tests for armor and weapon up to respective non-failure levels

--- enhancement.test.js
+++ enhancement.test.js
@@ -1,14 +1,14 @@
 const enhancement = require('./enhancement.js');
 
 describe('enhancement success', () => {
+    test('maximum enhancement is level 20 (PEN)', () => {
+        expect(enhancement.success({enhancement: 20})).toEqual({enhancement: 20});
+    }); 
     test('enhancement level increases by 1', () => {
         expect(enhancement.success({enhancement: 1})).toEqual({enhancement: 2});
     }); 
     test('name updated to reflect new success level', () => {
         expect(enhancement.success({name: '[DUO]itemName', enhancement: 17})).toEqual({name: '[TRI]itemName', enhancement: 18});
-    }); 
-    test('maximum enhancement is level 20 (PEN)', () => {
-        expect(enhancement.success({enhancement: 20})).toEqual({enhancement: 20});
     }); 
     test('enhancement display changes from numerals to PRI at enhancement level 16', () => {
         expect(enhancement.success({name: '[+15]itemName', enhancement: 15})).toEqual({name: '[PRI]itemName', enhancement: 16});
@@ -16,8 +16,13 @@ describe('enhancement success', () => {
 });
 
 
-describe('enhancement failure', () => {
-
+describe('enhancement fail', () => {
+    test('enhancing armor up to 5 cannot fail', () => {
+        expect(enhancement.fail({type: 'armor', enhancement: 4})).toEqual({type: 'armor', enhancement: 5});
+    });
+    test('enhancing weapon up to 7 cannot fail', () => {
+        expect(enhancement.fail({type: 'weapon', enhancement: 6})).toEqual({type: 'weapon', enhancement: 7});
+    });
 });
 
 describe('item repair', () => {
