f0637d4f39e94a9cb97b9ef93d198af9910c2223
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-01-30 18:53:31 -0700

    Wrote tests for max enhancement level and for change of display name for success function

--- enhancement.test.js
+++ enhancement.test.js
@@ -5,9 +5,16 @@ describe('enhancement success', () => {
         expect(enhancement.success({enhancement: 1})).toEqual({enhancement: 2});
     }); 
     test('name updated to reflect new success level', () => {
-        expect(enhancement.success({name: '[DUO]itemName'})).toEqual({name: '[TRI]itemName'});
+        expect(enhancement.success({name: '[DUO]itemName', enhancement: 17})).toEqual({name: '[TRI]itemName', enhancement: 18});
+    }); 
+    test('maximum enhancement is level 20 (PEN)', () => {
+        expect(enhancement.success({enhancement: 20})).toEqual({enhancement: 20});
+    }); 
+    test('enhancement display changes from numerals to PRI at enhancement level 16', () => {
+        expect(enhancement.success({name: '[+15]itemName', enhancement: 15})).toEqual({name: '[PRI]itemName', enhancement: 16});
     }); 
 });
+
 
 describe('enhancement failure', () => {
 
