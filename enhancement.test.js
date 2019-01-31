f8f7b666ff67d0062d1e9e38a67e0905d3fc99c7
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-01-30 19:39:34 -0700

    Added success test for enhancement level 0 and failure test for durability according to level

--- enhancement.test.js
+++ enhancement.test.js
@@ -1,10 +1,13 @@
 const enhancement = require('./enhancement.js');
 
 describe('enhancement success', () => {
+    test('enhancement level 0 displays nothing before name', () => {
+        expect(enhancement.success({enhancement: 0, name: 'itemName'}).toEqual({enhancement: 1, name:'[+1]itemName'}));
+    });
     test('maximum enhancement is level 20 (PEN)', () => {
         expect(enhancement.success({enhancement: 20})).toEqual({enhancement: 20});
     }); 
-    test('enhancement level increases by 1', () => {
+    test('success causes enhancement increase by 1', () => {
         expect(enhancement.success({enhancement: 1})).toEqual({enhancement: 2});
     }); 
     test('name updated to reflect new success level', () => {
@@ -18,10 +21,16 @@ describe('enhancement success', () => {
 
 describe('enhancement fail', () => {
     test('enhancing armor up to 5 cannot fail', () => {
-        expect(enhancement.fail({type: 'armor', enhancement: 4})).toEqual({type: 'armor', enhancement: 5});
+        expect(enhancement.fail({type: 'armor', enhancement: 4})).toEqual({type: 'armor', enhancement: 6});
     });
     test('enhancing weapon up to 7 cannot fail', () => {
         expect(enhancement.fail({type: 'weapon', enhancement: 6})).toEqual({type: 'weapon', enhancement: 7});
+    });
+    test('durability cannot be less than 20 when the enhancement between 0 and 14', () => {
+        expect(enhancement.fail({enhancement: 13, durability: 20})).toEqual({enhancement: 13, durability: 20});
+    });
+    test('durability cannot be less than 0 when the enhancement between 15 and 19', () => {
+        expect(enhancement.fail({enhancement: 16, durability: 0})).toEqual({enhancement: 16, durability: 0});
     });
 });
 
