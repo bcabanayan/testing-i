a46d36ca9ffd17844abadd5b3502f269e6bb25d7
Bruce Cabanayan <bcabanayan@Unknown.domain>
2019-01-30 21:59:10 -0700

    Added durability if statements to success function and durability property to success test objects

--- enhancer.js
+++ enhancer.js
@@ -3,38 +3,40 @@
 module.exports = {
     success: (item) => {
         if (item.enhancement <= 19) {
-            item.enhancement = item.enhancement + 1;
-            if (item.enhancement >= 1 && item.enhancement <= 15) {
-                let namePrefix = '[+' + item.enhancement.toString() + ']';
+            if ((item.enhancement <= 14 && item.durability >= 25) || (item.enhancement > 14 && item.durability >= 10)) {
+                item.enhancement = item.enhancement + 1;
+                if (item.enhancement >= 1 && item.enhancement <= 15) {
+                    let namePrefix = '[+' + item.enhancement.toString() + ']';
+                    item.namePrefix = namePrefix;
+                    item.name = namePrefix + item.baseName;
+                }
+                else if (item.enhancement === 16) {
+                    let namePrefix = '[PRI]';
+                    item.namePrefix = namePrefix;
+                    item.name = namePrefix + item.baseName;
+                }
+                else if (item.enhancement === 17) {
+                    let namePrefix = '[DUO]';
+                    item.namePrefix = namePrefix;
+                    item.name = namePrefix + item.baseName;
+                }
+                else if (item.enhancement === 18) {
+                    let namePrefix = '[TRI]';
+                    item.namePrefix = namePrefix;
+                    item.name = namePrefix + item.baseName;
+                }
+                else if (item.enhancement === 19) {
+                    let namePrefix = '[TET]';
+                    item.namePrefix = namePrefix;
+                    item.name = namePrefix + item.baseName;
+                }
+            }
+            else if (item.enhancement === 20) {
+                let namePrefix = '[PEN]';
                 item.namePrefix = namePrefix;
                 item.name = namePrefix + item.baseName;
             }
-            else if (item.enhancement === 16) {
-                let namePrefix = '[PRI]';
-                item.namePrefix = namePrefix;
-                item.name = namePrefix + item.baseName;
             }
-            else if (item.enhancement === 17) {
-                let namePrefix = '[DUO]';
-                item.namePrefix = namePrefix;
-                item.name = namePrefix + item.baseName;
-            }
-            else if (item.enhancement === 18) {
-                let namePrefix = '[TRI]';
-                item.namePrefix = namePrefix;
-                item.name = namePrefix + item.baseName;
-            }
-            else if (item.enhancement === 19) {
-                let namePrefix = '[TET]';
-                item.namePrefix = namePrefix;
-                item.name = namePrefix + item.baseName;
-            }
-        }
-        else if (item.enhancement === 20) {
-            let namePrefix = '[PEN]';
-            item.namePrefix = namePrefix;
-            item.name = namePrefix + item.baseName;
-        }
         return item;
     },
 
@@ -43,6 +45,7 @@ module.exports = {
     },
 
     repair: (item) => {
-
+        item.durability = 100;
+        return item;
     }
 }
